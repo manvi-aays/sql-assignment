c0ee33ad58e04d13a67917a57e2ffccea9e09881
manvi-aays <manvitiwari@aaysinsight.com>
2024-08-01 14:01:04 +0530

    sql assignment dated 1st August 2024

---
+++ sqlassignment2.sql
@@ -0,0 +1,22 @@
+use employees;
+
+SELECT CONCAT(first_name, ' ', last_name) AS name, gender 
+from employees 
+where first_name in('Aamod' , 'Arve') and last_name in ('Radwan', 'Erde')
+limit 2;
+
+select title, count(*) as count
+from titles
+group by title
+having title in ("Senior Engineer", "Assistant Engineer");
+
+SELECT 
+    CASE 
+        WHEN salary < 50000 THEN '<50000'
+        WHEN salary BETWEEN 50000 AND 100000 THEN '50000-100000'
+        ELSE '>100000'
+    END AS SalaryRange,
+    AVG(salary) AS AverageSalary
+FROM salaries
+GROUP BY SalaryRange;
+
